0a4e9c6ee9ebc4bbfe231f24975fb77fe980b10e
Gperez88 <perezgabriel89@gmail.com>
2023-09-28 13:49:12 -0400

    .

--- lib/src/utilities/file_util.dart
+++ lib/src/utilities/file_util.dart
@@ -1,6 +1,6 @@
 class FileUtil {
-  static bool isThatVideo(path) {
+  static bool isThatVideo(String path) {
     final reg = RegExp(r'(?:3gp)|(?:mp4)|(?:webm)|(?:mov)|(?:mkv)');
-    return path.contains(reg, path.length - 5);
+    return path.toLowerCase().contains(reg, path.length - 5);
   }
 }
